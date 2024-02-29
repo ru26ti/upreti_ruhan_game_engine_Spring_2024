02f90cba912de764d86ff759f8ce40dc9f2f3e69
ru26ti <160263596+ru26ti@users.noreply.github.com>
2024-02-29 12:52:03 -0800

    Push from Vs Code

--- main.py
+++ main.py
@@ -1,47 +1,63 @@
-# This file was created by: Ruhan Upreti
- 
-# Libraries imported here
+# This file was created by: Chris Cozort
+
+# import libraries and modules
 import pygame as pg
 from settings import *
 from sprites import *
-import sys
 from random import randint
+import sys
 from os import path
- 
-# Game class is created here
+
+from math import floor
+
+class Cooldown():
+    #set all properties to zero wehn instantiated
+    def _init_(self):
+        self.current_time = 0
+        self.event_time = 0
+        self.delta = 0
+        #ticking ensure the timer is counting
+    #must use ticking to count up ot donw
+    def ticking(self):
+        self.current_time = floor()
+
+
+# Define game class...
 class Game:
-    # Methods
+    # Define a special method to init the properties of said class...
     def __init__(self):
+        # init pygame
         pg.init()
-        # Sets the mode of the screen to width and height
+        # set size of screen and be the screen
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-        # writes text on the display
-        pg.display.set_caption("My First Video Game")
-        # sets the clock
+        pg.display.set_caption(TITLE)
+        # setting game clock 
         self.clock = pg.time.Clock()
-        pg.key.set_repeat(500, 100)
-        self.running = True
-        # Game info will be stored here later on
         self.load_data()
     def load_data(self):
         game_folder = path.dirname(__file__)
+        img_folder = path.join(game_folder, 'images')
+        self.player_img = pg.image.load(path.join(img_folder, 'sonic.png')).convert_alpha()
         self.map_data = []
         '''
-        With statement - content manager
-        Used to make sure resources are closed properly
-        after they are used. Can prevent errors.
+        The with statement is a context manager in Python. 
+        It is used to ensure that a resource is properly closed or released 
+        after it is used. This can help to prevent errors and leaks.
         '''
-        #opens the folder map.txt
         with open(path.join(game_folder, 'map.txt'), 'rt') as f:
             for line in f:
                 print(line)
                 self.map_data.append(line)
-                print(self.map_data)
+
+    # Create run method which runs the whole GAME
     def new(self):
+        print("create new game...")
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
-        #self.player = Player(self, 10, 10)
-       # self.all_sprites.add(self.player)
+        self.coins = pg.sprite.Group()
+        self.mobs = pg.sprite.Group()
+        self.power_ups = pg.sprite.Group()
+        # self.player1 = Player(self, 1, 1)
         # for x in range(10, 20):
         #     Wall(self, x, 5)
         for row, tiles in enumerate(self.map_data):
@@ -51,72 +67,70 @@ class Game:
                 if tile == '1':
                     print("a wall at", row, col)
                     Wall(self, col, row)
-                if tile == 'p':
+                if tile == 'P':
                     self.player = Player(self, col, row)
-                if tile == 'e':
-                    self.enemy = Enemy(self, col, row)
-#defines when the game is running
+                if tile == 'C':
+                    Coin(self, col, row)
+                if tile == 'M':
+                    Mob(self, col, row)
+                if tile == 'U':
+                    PowerUp(self, col, row)
+
     def run(self):
+        # 
         self.playing = True
         while self.playing:
             self.dt = self.clock.tick(FPS) / 1000
-            # input
             self.events()
-            #  processing
             self.update()
-            #  output
             self.draw()
-#defs the quit function
     def quit(self):
-        pg.quit()
-        sys.exit()
-    # methods
-    def input(self):
-        pass
+         pg.quit()
+         sys.exit()
+
     def update(self):
         self.all_sprites.update()
-   #draws the grid of the screen
+    
     def draw_grid(self):
-        for x in range(0, WIDTH, TILESIZE):
-            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
-        for y in range(0, HEIGHT, TILESIZE):
-            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
-#sets the color of the screen
+         for x in range(0, WIDTH, TILESIZE):
+              pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+         for y in range(0, HEIGHT, TILESIZE):
+              pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+    
+    def draw_text(self, surface, text, size, color, x, y):
+        font_name = pg.font.match_font('arial')
+        font = pg.font.Font(font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.topleft = (x,y)
+        surface.blit(text_surface, text_rect)
+    
     def draw(self):
-        self.screen.fill(BGCOLOR)
-        self.draw_grid()
-        self.all_sprites.draw(self.screen)
-        pg.display.flip()
- 
+            self.screen.fill(BGCOLOR)
+            self.draw_grid()
+            self.all_sprites.draw(self.screen)
+            self.draw_text(self.screen, str(self.test_timer.countdown(45)), 24, WHITE, WIDTH/2 - 32, 2)
+            pg.display.flip()
+
     def events(self):
-            # listen for events on the keyboard here
-            for event in pg.event.get():
-                # whit red x = game closes
-                if event.type == pg.QUIT:
-                    self.quit()
-                    print("the game has ended..")
-                # controls (keyboard events)
-                # if event.type == pg.KEYDOWN:
-                #     if event.key == pg.K_LEFT:
-                #         self.player.move(dx=-1)
-                # if event.type == pg.KEYDOWN:
-                #     if event.key == pg.K_RIGHT:
-                #         self.player.move(dx=1)
-                # if event.type == pg.KEYDOWN:
-                #     if event.key == pg.K_UP:
-                #         self.player.move(dy=-1)
-                # if event.type == pg.KEYDOWN:
-                #     if event.key == pg.K_DOWN:
-                #         self.player.move(dy=1)
-    def show_start_screen(self):
-        pass
-    def show_go_screen(self):
-        pass
-#Activates Game
+         for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            # if event.type == pg.KEYDOWN:
+            #     if event.key == pg.K_LEFT:
+            #         self.player.move(dx=-1)
+            #     if event.key == pg.K_RIGHT:
+            #         self.player.move(dx=1)
+            #     if event.key == pg.K_UP:
+            #         self.player.move(dy=-1)
+            #     if event.key == pg.K_DOWN:
+            #         self.player.move(dy=1)
+
+# Instantiate the game... 
 g = Game()
-# g.show_go_screen()
+# use game method run to run
+# g.show_start_screen()
 while True:
     g.new()
     g.run()
     # g.show_go_screen()
- 
